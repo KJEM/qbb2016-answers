8e87a2e3886c6a60293e085ecb0cf52c5be3173b
Kaya Matson <matson.kaya@gmail.com>
2016-08-30 13:02:45 -0400

    Did first basic exercise

---
+++ day2-morning/day2-exercise-#.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+f = open ( "/Users/cmdb/data/893.sam" )
+count = 0
+ 
+
+for line in f:
+    if line.startswith( "SRR" ):
+        count = count + 1
+        
+        
+print count    
