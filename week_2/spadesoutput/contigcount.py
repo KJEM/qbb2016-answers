0b5d517bd677b04daf465542def8ee231abb39b8
Kaya Matson <matson.kaya@gmail.com>
2016-09-16 17:32:31 -0400

    Python code for getting min, max, average contig values

---
+++ week_2/spadesoutput/contigcount.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+
+""""
+Read sequences from a fasta file, count the number of times each 
+k-mer occurs acroos all sequences and print kmers and counts.
+
+usage: 01-kmer-counter.py k < fasta_file
+"""
+
+import sys 
+import math
+import fasta
+
+file = open("contigs.fasta", "r")
+string_list = file.read().split(">")
+length = []
+# for = open("listoflengths", "w")
+
+for i in range(1, len(string_list)):
+      length.append(float(string_list[i].split("_")[3]))
+# print length
+
+print "minimum:"
+print str(min(length))
+print "maximum:"
+print str(max(length))
+print "average:"
+avg = str(sum(length) / float(len(length)))
+print avg
+#
+#
+# print "halfway point:"
+# half = str(sum(length) / float(2))
+# print half
+
+# print length
+
+# sortedlist = sort()
+# print sortedlist
+#
